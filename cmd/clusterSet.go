45c7368ff97f9c372994e6b7b86af0c85d5779f5
Salvatore Fasano <fasanosalvatore@hotmail.it>
2022-07-02 19:24:52 +0200

    feat: add support for cluster set default operation

--- cmd/clusterSet.go
+++ cmd/clusterSet.go
@@ -6,19 +6,43 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-var setCmd = &cobra.Command{
-	Use:   "set [name of the cluster]",
-	Short: "The cluster set command allows you to set a new default cluster",
-	Long:  `The cluster set command allows you to set a new default cluster from the list of those you have access to.`,
-	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Cluster Set Called")
-	},
-}
+var (
+	setCmd = &cobra.Command{
+		Use:   "set [name of the cluster]",
+		Short: "The cluster set command allows you to set a new default cluster",
+		Long:  `The cluster set command allows you to set a new default cluster from the list of those you have access to.`,
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			newDefaultClusterName := args[0]
+
+			var clusters []map[string]interface{}
+			getClusters(&clusters)
+
+			isClusterPresent := false
+			for _, cluster := range clusters {
+				if cluster["clusterName"] == newDefaultClusterName {
+					isClusterPresent = true
+					break
+				}
+			}
+
+			if !isClusterPresent {
+				fmt.Printf("Error: Cluster %s not found.\n", newDefaultClusterName)
+				os.Exit(0)
+			}
+
+			viper.Set("defaultCluster", newDefaultClusterName)
+			viper.WriteConfig()
+			fmt.Printf("Cluster %s is the new default cluster.\n", newDefaultClusterName)
+		},
+	}
+)
 
 func init() {
 	clusterCmd.AddCommand(setCmd)
