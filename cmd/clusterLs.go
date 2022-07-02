deee126af517c39d7d1ac222fb76a45d2abfa6cd
Salvatore Fasano <fasanosalvatore@hotmail.it>
2022-07-02 19:06:13 +0200

    feat: add support for cluster ls operation

--- cmd/clusterLs.go
+++ cmd/clusterLs.go
@@ -6,8 +6,11 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var lsClusterCmd = &cobra.Command{
@@ -15,7 +18,21 @@ var lsClusterCmd = &cobra.Command{
 	Short: "The cluster ls command allows you to view the list of your clusters",
 	Long:  `The cluster ls command allows you to view the list of clusters to which you have access.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Cluster Ls Called")
+		var clusters []map[string]interface{}
+		getClusters(&clusters)
+		defaultCluster := viper.Get("defaultCluster")
+
+		w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
+		fmt.Fprintln(w, "NAME\tURL\tUSERNAME\tDEFAULT\t")
+		for _, cluster := range clusters {
+			if cluster["clusterName"] == defaultCluster {
+				fmt.Fprintf(w, "%s\t%s\t%s\tx\t\n", cluster["clusterName"], cluster["clusterUrl"], cluster["userEmail"])
+			} else {
+				fmt.Fprintf(w, "%s\t%s\t%s\t\t\n", cluster["clusterName"], cluster["clusterUrl"], cluster["userEmail"])
+			}
+		}
+		w.Flush()
+
 	},
 }
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/flaky-infra/flaky-cli
 go 1.17
 
 require (
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
